b2eb148dc309385d6dd62fb5d5027b9a246f696c
Abraham Ticona <keima31@hotmail.com>
2015-04-15 22:16:05 +0200

    Svolte consegne

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Definisci le costanti
+#define N 3
+#define MAX_STRLEN 30
 
-// Definisci il tipo di dato struct s_studente
+struct s_studente{
+    char nome[MAX_STRLEN];
+    char cognome[MAX_STRLEN];
+    int eta;
+    char classe[MAX_STRLEN];
+};
 
-// Definisci studente come struct s_studente
+typedef struct s_studente studente;
 
 int main(int argc, char** argv) {
-    // Dichiara le variabili locali
+    int i;
+    studente v[N];
     
-    // Codice per l'inserimento, da parte dell'utente, dei dati
-    
-    // Codice per la stampa dei dati (uno studente per riga)
+    for(i=0;i<N;i++)
+    {
+        printf("inserisci il nome: ");
+        scanf("%s", v[i].nome);
+        printf("inserisci il cognome: ");
+        scanf("%s", v[i].cognome);
+        printf("inserisci l'eta': ");
+        scanf("%d", &v[i].eta);
+        printf("inserisci la classe: ");
+        scanf("%s", v[i].classe);
+       
+    }
+     
+    for(i=0;i<N;i++)
+    {
+      printf("%s; %s; %d; %s\n",v[i].nome,v[i].cognome,v[i].eta,v[i].classe);
+    }
     
     return (EXIT_SUCCESS);
 }
-
